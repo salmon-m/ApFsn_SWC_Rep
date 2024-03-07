ae3200442b353d21684bf97fde241cd357f3f705
salmon-m <m.29.migi@gmail.com>
2024-03-07 17:40:33 +0900

    main.cpp 訂正版 main_schedule()を入力、変数のスコープ変更

--- ApFsn_SoftComp/ApFsn_mainComp.h
+++ ApFsn_SoftComp/ApFsn_mainComp.h
@@ -1,6 +1,5 @@
 #pragma once
-#include "typeDifference.h"
 
 extern void vdg_main_init(void);
 extern void vdg_main_4cycle(void);
-extern void main_1(void);
+extern void main_schedule(void);
--- ApFsn_SoftComp/main.cpp
+++ ApFsn_SoftComp/main.cpp
@@ -1,9 +1,51 @@
-#include "ApFsn_mainComp.h"
 #include "typeDifference.h"
 
+#define ON ((U1)0)
+#define OFF ((U1)1)
 
-S4 main()
+static U1 u1s_main_CnInitial = ON;
+static U1 u1s_main_InCnt = 0;
+static U1 u1s_main_Cn4Cycle = OFF;
+
+void vdg_main_init() {
+	return;
+}
+
+void vdg_main_4cycle() {
+	return;
+}
+
+void main_schedule(void)
 {
-	main_1();
-	return 0;
+	//初回フラグ
+	if (u1s_main_CnInitial == ON)
+		{
+			vdg_main_init();
+			u1s_main_CnInitial = OFF;
+		}
+	else
+	{
+		//カウント
+		u1s_main_InCnt++;
+		u1s_main_InCnt %= 4;
+		//カウント数確認
+		if ((u1s_main_InCnt % 4) == 0)
+		{
+			u1s_main_Cn4Cycle = ON;
+		}
+		else
+		{
+			u1s_main_Cn4Cycle = OFF;
+		}
+		//定周期フラグ
+		if (u1s_main_Cn4Cycle == ON)
+		{
+			vdg_main_4cycle();
+		}
+		//elseは処理無し
+		else {
+		}
+
+	}
+	return;
 }
