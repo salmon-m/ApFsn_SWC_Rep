923029ac7390392c4d0b6267c9500d860017c05f
salmon-m <m.29.migi@gmail.com>
2024-03-07 15:56:27 +0900

    main.ccpに統一していないが一旦commit 2回目

--- ApFsn_SoftComp/main.cpp
+++ ApFsn_SoftComp/main.cpp
@@ -1,19 +1,9 @@
+#include "ApFsn_mainComp.h"
 #include "typeDifference.h"
 
 
-#define ON ((U1)0)
-#define OFF ((U1)1)
-
-void vdg_main_init() {
-	return;
-}
-
-void vdg_main_4cycle() {
-	return;
-}
-
-
-void main(void)
+S4 main()
 {
-	return;
+	main_1();
+	return 0;
 }
