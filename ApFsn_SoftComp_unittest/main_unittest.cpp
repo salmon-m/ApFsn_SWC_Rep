c8affa7634f493b2992068359d1bc028e3c3bc9c
salmon-m <m.29.migi@gmail.com>
2024-03-08 16:54:19 +0900

    ユニットテストのコードを修正 　・不要なコードの削除 　・vdg_main_init()とvdg_main_4cycle()のユニットテストを各1つに修正

--- ApFsn_SoftComp_unittest/main_unittest.cpp
+++ ApFsn_SoftComp_unittest/main_unittest.cpp
@@ -6,7 +6,6 @@
 TEST(TestCaseMainSchedule, TestMainSchedule1) {
 	main_schedule();
 	EXPECT_EQ(0, u1s_main_CnInitial);
-	EXPECT_TRUE(true);
 }
 
 TEST(TestCaseMainSchedule, TestMainSchedule2) {
@@ -14,7 +13,6 @@ TEST(TestCaseMainSchedule, TestMainSchedule2) {
 	u1s_main_InCnt = 0;
 	main_schedule();
 	EXPECT_EQ(1, u1s_main_InCnt);
-	EXPECT_TRUE(true);
 }
 
 TEST(TestCaseMainSchedule, TestMainSchedule3) {
@@ -22,43 +20,20 @@ TEST(TestCaseMainSchedule, TestMainSchedule3) {
 	u1s_main_InCnt = 3;
 	main_schedule();
 	EXPECT_EQ(1, u1s_main_Cn4Cycle);
-	EXPECT_TRUE(true);
 }
 
 //main_init()のユニットテスト
-TEST(TestCaseMainInit, TestMainInit1) {
+TEST(TestCaseMainInit, TestMainInit) {
 	vdg_main_init();
 	EXPECT_EQ(1, u1s_main_InLyrResult);
-	EXPECT_TRUE(true);
-}
-
-TEST(TestCaseMainInit, TestMainInit2) {
-	vdg_main_init();
 	EXPECT_EQ(1, u1s_main_PrcResult);
-	EXPECT_TRUE(true);
-}
-
-TEST(TestCaseMainInit, TestMainInit3) {
-	vdg_main_init();
 	EXPECT_EQ(1, u1s_main_OutLyrResult);
-	EXPECT_TRUE(true);
 }
 
 //vdg_main_4cycle()のユニットテスト
 TEST(TestCaseMain4cycle, TestMain4cycle1) {
 	vdg_main_4cycle();
 	EXPECT_EQ(2, u1s_main_InLyrResult);
-	EXPECT_TRUE(true);
-}
-
-TEST(TestCaseMain4cycle, TestMain4cycle2) {
-	vdg_main_4cycle();
 	EXPECT_EQ(2, u1s_main_PrcResult);
-	EXPECT_TRUE(true);
-}
-
-TEST(TestCaseMain4cycle, TestMain4cycle3) {
-	vdg_main_4cycle();
 	EXPECT_EQ(2, u1s_main_OutLyrResult);
-	EXPECT_TRUE(true);
 }
