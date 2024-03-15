8853d337a0ccc846eb6ee63e2007aea0b1500f33
Mao Migita <m.29.migi@gmail.com>
2024-03-15 16:39:51 +0900

    clangフォーマッターを適用

--- ApFsn_SoftComp/main.cpp
+++ ApFsn_SoftComp/main.cpp
@@ -3,50 +3,48 @@
 #include "InLyr.h"
 #include "Prc.h"
 
-//Dummy定義
-//本当は main.cppの外部入力になるが、
-//ApFsn_SoftComp.cppではmain()定義しており、
-//main()をリンカー入力するとUnittest.exeがmain()を呼んでしまうため
-//やむえず main.cpp に外部入力を定義
+// Dummy定義
+// 本当は main.cppの外部入力になるが、
+// ApFsn_SoftComp.cppではmain()定義しており、
+// main()をリンカー入力するとUnittest.exeがmain()を呼んでしまうため
+// やむえず main.cpp に外部入力を定義
 U1 u1g_ApFsn_IG;
 st_csr_clstr stg_csr_dtct_clstr[3];
 st_pvm_clstr stg_pvm_dtct_clstr[4];
-//Dummy定義
+// Dummy定義
 
+static U1 u1s_main_InCnt;	 // カウント数
+static U1 u1s_main_Cn4Cycle; // 定周期フラグ
+/* ここからUnitTest用の変数宣言
+ *static U1 u1s_main_InLyrResult = (U1)0;
+ *static U1 u1s_main_PrcResult = (U1)0;
+ *static U1 u1s_main_OutLyrResult = (U1)0;
+ * ここまでUnitTest用 */
 
-static U1 u1s_main_InCnt;            //カウント数
-static U1 u1s_main_Cn4Cycle;         //定周期フラグ
-/* ここからUnitTest用の変数宣言 */
-//static U1 u1s_main_InLyrResult = (U1)0;
-//static U1 u1s_main_PrcResult = (U1)0;
-static U1 u1s_main_OutLyrResult = (U1)0;
-/* ここまでUnitTest用 */
-
-//プロトタイプ宣言
+// プロトタイプ宣言
 void vdg_main_init(void);
 void vdg_main_4cycle(void);
-/* ここからUnitTest用のプロトタイプ宣言 */
-//void vdg_InLyr_init(void);
-//void vdg_Prc_init(void);
-void vdg_OutLyr_init(void);
-//void vdg_InLyr_GlobalCapture(void);
-//void vdg_Prc_GridUpdate(void);
-void vdg_OutLyr_GlobalUpdate(void);
-/* ここまでUnitTest用 */
-
+/* ここからUnitTest用のプロトタイプ宣言
+ *void vdg_InLyr_init(void);
+ *void vdg_Prc_init(void);
+ *void vdg_OutLyr_init(void);
+ *void vdg_InLyr_GlobalCapture(void);
+ *void vdg_Prc_GridUpdate(void);
+ *void vdg_OutLyr_GlobalUpdate(void);
+ * ここまでUnitTest用 */
 
 void main_schedule(void)
 {
-	//初回フラグ
+	// 初回フラグ
 	if (u1g_ApFsn_IG == ON)
-		{
-			vdg_main_init();
-		}
+	{
+		vdg_main_init();
+	}
 	else
 	{
-		//カウント
+		// カウント
 		u1s_main_InCnt++;
-		//カウント数確認
+		// カウント数確認
 		if ((u1s_main_InCnt % 4) == 0)
 		{
 			u1s_main_Cn4Cycle = ON;
@@ -56,26 +54,27 @@ void main_schedule(void)
 		{
 			u1s_main_Cn4Cycle = OFF;
 		}
-		//定周期フラグ
+		// 定周期フラグ
 		if (u1s_main_Cn4Cycle == ON)
 		{
 			vdg_main_4cycle();
 		}
-		//elseは処理無し
-		else {
+		// elseは処理無し
+		else
+		{
 		}
-
 	}
 	return;
 }
 
 // コンポの初期化
-void vdg_main_init(void) {
-	//自モジュールのRAM初期化
+void vdg_main_init(void)
+{
+	// 自モジュールのRAM初期化
 	u1s_main_InCnt = (U1)0;
 	u1s_main_Cn4Cycle = OFF;
 
-	//他モジュールの初期化関数コール
+	// 他モジュールの初期化関数コール
 	vdg_InLyr_init();
 	vdg_Prc_init();
 	vdg_OutLyr_init();
@@ -83,8 +82,9 @@ void vdg_main_init(void) {
 	return;
 }
 
-//定周期で処理を実行
-void vdg_main_4cycle(void) {
+// 定周期で処理を実行
+void vdg_main_4cycle(void)
+{
 	vdg_InLyr_GlobalCapture();
 	vdg_Prc_GridUpdate();
 	vdg_OutLyr_GlobalUpdate();
@@ -92,32 +92,31 @@ void vdg_main_4cycle(void) {
 }
 
 /* ここからUnitTest用 */
-//初期化処理
+// 初期化処理
 /*
-*void vdg_InLyr_init(void) {
-*	u1s_main_InLyrResult = 1;
-*}
-*
-*void vdg_Prc_init(void) {
-*	u1s_main_PrcResult = 1;
-*}
-*/
-void vdg_OutLyr_init(void) {
-	u1s_main_OutLyrResult = 1;
-}
-
-//定周期処理
-/*
-*void vdg_InLyr_GlobalCapture(void) {
-*	u1s_main_InLyrResult = 2;
-*}
-*
-*void vdg_Prc_GridUpdate(void) {
-*	u1s_main_PrcResult = 2;
-*}
-*/
-void vdg_OutLyr_GlobalUpdate(void) {
-	u1s_main_OutLyrResult = 2;
-}
-/* ここまでUnitTest用 */
-
+ *void vdg_InLyr_init(void) {
+ *	u1s_main_InLyrResult = 1;
+ *}
+ *
+ *void vdg_Prc_init(void) {
+ *	u1s_main_PrcResult = 1;
+ *}
+ *
+ *void vdg_OutLyr_init(void) {
+ *	u1s_main_OutLyrResult = 1;
+ *}
+ *
+ *定周期処理
+ *
+ *void vdg_InLyr_GlobalCapture(void) {
+ *	u1s_main_InLyrResult = 2;
+ *}
+ *
+ *void vdg_Prc_GridUpdate(void) {
+ *	u1s_main_PrcResult = 2;
+ *}
+ *
+ *void vdg_OutLyr_GlobalUpdate(void) {
+ *	u1s_main_OutLyrResult = 2;
+ *}
+ * ここまでUnitTest用 */
--- ApFsn_SoftComp/main.h
+++ ApFsn_SoftComp/main.h
@@ -1,13 +1,12 @@
 #include "typeDifference.h"
 #include "structureDeclaration.h"
 
-//公開関数
+// 公開関数
 extern void vdg_main_init(void);
 extern void vdg_main_4cycle(void);
 extern void main_schedule(void);
 
-
-//Dummy：ApFsn_SoftCompの外部入力になるが、mainで定義して動かす
+// Dummy：ApFsn_SoftCompの外部入力になるが、mainで定義して動かす
 extern U1 u1g_ApFsn_IG;
 extern st_csr_clstr stg_csr_dtct_clstr[3];
 extern st_pvm_clstr stg_pvm_dtct_clstr[4];
