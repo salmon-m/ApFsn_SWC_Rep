a679ca64ba838ea10468abce653e871b973b1930
Mao Migita <m.29.migi@gmail.com>
2024-03-15 09:37:33 +0900

    vdg_OutLyr_GlobalUpdate()に対してのユニットテストを追加

--- ApFsn_SoftComp_unittest/OutLyr_unittest.cpp
+++ ApFsn_SoftComp_unittest/OutLyr_unittest.cpp
@@ -1,5 +1,6 @@
 #include "pch.h"
 #include "OutLyr.h"
+#include "InLyr.h"
 #include "OutLyr.cpp"
 
 //vdg_InLyr_init()のユニットテスト
@@ -14,3 +15,52 @@ TEST(TestCaseOutLyrInit, TestOutLyrInit) {
 	EXPECT_EQ(0, stg_OutLyr_ArGridInfo[900][3500].CnObjSet);
 }
 
+//車の後方、物体検知あり
+TEST(TestCaseOutLyrGU, BehindTheCar_ConfObj) {
+
+	stg_Prc_ArGridInfo[0][0].CnSnrDtct = 1;
+	stg_Prc_ArGridInfo[0][0].CnSodDtct = 1;
+	stg_Prc_ArGridInfo[0][0].InOcpy = 20;
+	stg_Prc_ArGridInfo[0][0].CnObjSet = 1;
+
+	vdg_OutLyr_GlobalUpdate();
+
+	EXPECT_EQ(1, stg_OutLyr_ArGridInfo[0][0].CnObjSet);
+}
+
+//車の後方、物体検知なし
+TEST(TestCaseOutLyrGU, BehindTheCar_NoConfObj) {
+
+	stg_Prc_ArGridInfo[0][0].CnSnrDtct = 0;
+	stg_Prc_ArGridInfo[0][0].CnSodDtct = 0;
+	stg_Prc_ArGridInfo[0][0].InOcpy = 0;
+	stg_Prc_ArGridInfo[0][0].CnObjSet = 0;
+
+	vdg_OutLyr_GlobalUpdate();
+
+	EXPECT_EQ(0, stg_OutLyr_ArGridInfo[0][0].CnObjSet);
+}
+
+//車の前方、物体検知あり
+TEST(TestCaseOutLyrGU, FrontOfTheCar_ConfObj) {
+	stg_Prc_ArGridInfo[900][3500].CnSnrDtct = 1;
+	stg_Prc_ArGridInfo[900][3500].CnSodDtct = 1;
+	stg_Prc_ArGridInfo[900][3500].InOcpy = 20;
+	stg_Prc_ArGridInfo[900][3500].CnObjSet = 1;
+
+	vdg_OutLyr_GlobalUpdate();
+
+	EXPECT_EQ(1, stg_OutLyr_ArGridInfo[900][3500].CnObjSet);
+}
+
+//車の前方、物体検知なし
+TEST(TestCaseOutLyrGU, FrontOfTheCar_NoConfObj) {
+	stg_Prc_ArGridInfo[900][3500].CnSnrDtct = 0;
+	stg_Prc_ArGridInfo[900][3500].CnSodDtct = 0;
+	stg_Prc_ArGridInfo[900][3500].InOcpy = 0;
+	stg_Prc_ArGridInfo[900][3500].CnObjSet = 0;
+
+	vdg_OutLyr_GlobalUpdate();
+
+	EXPECT_EQ(0, stg_OutLyr_ArGridInfo[900][3500].CnObjSet);
+}
