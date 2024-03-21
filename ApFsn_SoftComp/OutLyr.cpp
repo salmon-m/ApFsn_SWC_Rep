18572871d935efe6ddf5b801f7e41b5c0d3993d0
Mao Migita <m.29.migi@gmail.com>
2024-03-21 14:44:33 +0900

    指摘No.1～6の対応

--- ApFsn_SoftComp/OutLyr.cpp
+++ ApFsn_SoftComp/OutLyr.cpp
@@ -11,14 +11,14 @@ st_GridMap_Out stg_OutLyr_ArGridInfo[1000][4000];
 // コンポの初期化
 void vdg_OutLyr_init(void)
 {
-	U2 U2_OutLyrI_LoopRow = (U2)0;
-	U2 U2_OutLyrI_LoopColumn = (U2)0;
+	U2 u2t_OutLyrI_LoopRow;
+	U2 u2t_OutLyrI_LoopColumn;
 
-	for (U2_OutLyrI_LoopRow = 0; U2_OutLyrI_LoopRow < 1000; U2_OutLyrI_LoopRow++)
+	for (u2t_OutLyrI_LoopRow = (U2)0; u2t_OutLyrI_LoopRow < (U2)1000; u2t_OutLyrI_LoopRow++)
 	{
-		for (U2_OutLyrI_LoopColumn = 0; U2_OutLyrI_LoopColumn < 4000; U2_OutLyrI_LoopColumn++)
+		for (u2t_OutLyrI_LoopColumn = (U2)0; u2t_OutLyrI_LoopColumn < (U2)4000; u2t_OutLyrI_LoopColumn++)
 		{
-			stg_OutLyr_ArGridInfo[U2_OutLyrI_LoopRow][U2_OutLyrI_LoopColumn].CnObjSet = 0;
+			stg_OutLyr_ArGridInfo[u2t_OutLyrI_LoopRow][u2t_OutLyrI_LoopColumn].CnObjSet = (U1)0;
 		}
 	}
 
@@ -28,14 +28,14 @@ void vdg_OutLyr_init(void)
 // 外部出力データの更新
 void vdg_OutLyr_GlobalUpdate(void)
 {
-	U2 U2_OutLyrGU_LoopRow = (U2)0;
-	U2 U2_OutLyrGU_LoopColumn = (U2)0;
+	U2 u2t_OutLyrGU_LoopRow;
+	U2 u2t_OutLyrGU_LoopColumn;
 
-	for (U2_OutLyrGU_LoopRow = 0; U2_OutLyrGU_LoopRow < 1000; U2_OutLyrGU_LoopRow++)
+	for (u2t_OutLyrGU_LoopRow = (U2)0; u2t_OutLyrGU_LoopRow < (U2)1000; u2t_OutLyrGU_LoopRow++)
 	{
-		for (U2_OutLyrGU_LoopColumn = 0; U2_OutLyrGU_LoopColumn < 4000; U2_OutLyrGU_LoopColumn++)
+		for (u2t_OutLyrGU_LoopColumn = (U2)0; u2t_OutLyrGU_LoopColumn < (U2)4000; u2t_OutLyrGU_LoopColumn++)
 		{
-			stg_OutLyr_ArGridInfo[U2_OutLyrGU_LoopRow][U2_OutLyrGU_LoopColumn].CnObjSet = stg_Prc_ArGridInfo[U2_OutLyrGU_LoopRow][U2_OutLyrGU_LoopColumn].CnObjSet;
+			stg_OutLyr_ArGridInfo[u2t_OutLyrGU_LoopRow][u2t_OutLyrGU_LoopColumn].CnObjSet = stg_Prc_ArGridInfo[u2t_OutLyrGU_LoopRow][u2t_OutLyrGU_LoopColumn].CnObjSet;
 		}
 	}
 
--- ApFsn_SoftComp/Prc.cpp
+++ ApFsn_SoftComp/Prc.cpp
@@ -11,17 +11,17 @@ st_GridMap stg_Prc_ArGridInfo[1000][4000]; // グリッドマップ情報
 // 初期化
 void vdg_Prc_init(void)
 {
-	U2 U2_PrcI_LoopRow = (U2)0;
-	U2 U2_PrcI_LoopColumn = (U2)0;
+	U2 u2t_PrcI_LoopRow;
+	U2 u2t_PrcI_LoopColumn;
 
-	for (U2_PrcI_LoopRow = 0; U2_PrcI_LoopRow < 1000; U2_PrcI_LoopRow++)
+	for (u2t_PrcI_LoopRow = (U2)0; u2t_PrcI_LoopRow < (U2)1000; u2t_PrcI_LoopRow++)
 	{
-		for (U2_PrcI_LoopColumn = 0; U2_PrcI_LoopColumn < 4000; U2_PrcI_LoopColumn++)
+		for (u2t_PrcI_LoopColumn = (U2)0; u2t_PrcI_LoopColumn < (U2)4000; u2t_PrcI_LoopColumn++)
 		{
-			stg_Prc_ArGridInfo[U2_PrcI_LoopRow][U2_PrcI_LoopColumn].CnSnrDtct = (U1)0;
-			stg_Prc_ArGridInfo[U2_PrcI_LoopRow][U2_PrcI_LoopColumn].CnSodDtct = (U1)0;
-			stg_Prc_ArGridInfo[U2_PrcI_LoopRow][U2_PrcI_LoopColumn].InOcpy = (U1)0;
-			stg_Prc_ArGridInfo[U2_PrcI_LoopRow][U2_PrcI_LoopColumn].CnObjSet = (U1)0;
+			stg_Prc_ArGridInfo[u2t_PrcI_LoopRow][u2t_PrcI_LoopColumn].CnSnrDtct = (U1)0;
+			stg_Prc_ArGridInfo[u2t_PrcI_LoopRow][u2t_PrcI_LoopColumn].CnSodDtct = (U1)0;
+			stg_Prc_ArGridInfo[u2t_PrcI_LoopRow][u2t_PrcI_LoopColumn].InOcpy = (U1)0;
+			stg_Prc_ArGridInfo[u2t_PrcI_LoopRow][u2t_PrcI_LoopColumn].CnObjSet = (U1)0;
 		}
 	}
 
@@ -30,53 +30,53 @@ void vdg_Prc_init(void)
 
 void vdg_Prc_GridUpdate(void)
 {
-	S1 S1_PrcGU_SnrClusterCount = (S1)0;
-	S1 S1_PrcGU_SnrPointCount = (S1)0;
-	S1 S1_PrcGU_SodClusterCount = (S1)0;
-	S1 S1_PrcGU_SodPointCount = (S1)0;
-	U2 U2_PrcGU_LoopRow = (U2)0;
-	U2 U2_PrcGU_LoopColumn = (U2)0;
-	S2 S2_PrcGU_X = (S2)0;
-	S2 S2_PrcGU_Y = (S2)0;
+	S1 s1t_PrcGU_SnrClusterCount;
+	S1 s1t_PrcGU_SnrCrdCount;
+	S1 s1t_PrcGU_SodClusterCount;
+	S1 s1t_PrcGU_SodCrdCount;
+	U2 u2t_PrcGU_LoopRow;
+	U2 u2t_PrcGU_LoopColumn;
+	S2 s2t_PrcGU_X = (S2)0;
+	S2 s2t_PrcGU_Y = (S2)0;
 
 	// ソナー
-	for (S1_PrcGU_SnrClusterCount = 0; (S1_PrcGU_SnrClusterCount < 3) && (stg_InLyr_SnrDtctClstr[S1_PrcGU_SnrClusterCount].InCrdNum != 0); S1_PrcGU_SnrClusterCount++)
+	for (s1t_PrcGU_SnrClusterCount = (S1)0; s1t_PrcGU_SnrClusterCount < (S1)3; s1t_PrcGU_SnrClusterCount++)
 	{
-		for (S1_PrcGU_SnrPointCount = 0; S1_PrcGU_SnrPointCount < stg_InLyr_SnrDtctClstr[S1_PrcGU_SnrClusterCount].InCrdNum; S1_PrcGU_SnrPointCount++)
+		for (s1t_PrcGU_SnrCrdCount = (S1)0; s1t_PrcGU_SnrCrdCount < stg_InLyr_SnrDtctClstr[s1t_PrcGU_SnrClusterCount].InCrdNum; s1t_PrcGU_SnrCrdCount++)
 		{
-			if (stg_InLyr_SnrDtctClstr[S1_PrcGU_SnrClusterCount].EmClstrState == 1)
+			if (stg_InLyr_SnrDtctClstr[s1t_PrcGU_SnrClusterCount].EmClstrState == (U1)1)
 			{
-				S2_PrcGU_X = stg_InLyr_SnrDtctClstr[S1_PrcGU_SnrClusterCount].st_crd[S1_PrcGU_SnrPointCount].In_X + 1000;
-				S2_PrcGU_Y = stg_InLyr_SnrDtctClstr[S1_PrcGU_SnrClusterCount].st_crd[S1_PrcGU_SnrPointCount].In_Y + 500;
-				stg_Prc_ArGridInfo[S2_PrcGU_Y][S2_PrcGU_X].CnSnrDtct = 1;
-				stg_Prc_ArGridInfo[S2_PrcGU_Y][S2_PrcGU_X].InOcpy += 5;
+				s2t_PrcGU_X = stg_InLyr_SnrDtctClstr[s1t_PrcGU_SnrClusterCount].st_crd[s1t_PrcGU_SnrCrdCount].In_X + (S2)1000;
+				s2t_PrcGU_Y = stg_InLyr_SnrDtctClstr[s1t_PrcGU_SnrClusterCount].st_crd[s1t_PrcGU_SnrCrdCount].In_Y + (S2)500;
+				stg_Prc_ArGridInfo[s2t_PrcGU_Y][s2t_PrcGU_X].CnSnrDtct = (U1)1;
+				stg_Prc_ArGridInfo[s2t_PrcGU_Y][s2t_PrcGU_X].InOcpy += (U1)5;
 			}
 		}
 	}
 
 	// PVM
-	for (S1_PrcGU_SodClusterCount = 0; (S1_PrcGU_SodClusterCount < 4) && (stg_InLyr_SodDtctClstr[S1_PrcGU_SodClusterCount].InCrdNum != 0); S1_PrcGU_SodClusterCount++)
+	for (s1t_PrcGU_SodClusterCount = (S1)0; s1t_PrcGU_SodClusterCount < (S1)4; s1t_PrcGU_SodClusterCount++)
 	{
-		for (S1_PrcGU_SodPointCount = 0; S1_PrcGU_SodPointCount < stg_InLyr_SodDtctClstr[S1_PrcGU_SodClusterCount].InCrdNum; S1_PrcGU_SodPointCount++)
+		for (s1t_PrcGU_SodCrdCount = (S1)0; s1t_PrcGU_SodCrdCount < stg_InLyr_SodDtctClstr[s1t_PrcGU_SodClusterCount].InCrdNum; s1t_PrcGU_SodCrdCount++)
 		{
-			if (stg_InLyr_SodDtctClstr[S1_PrcGU_SodClusterCount].EmClstrState == 1)
+			if (stg_InLyr_SodDtctClstr[s1t_PrcGU_SodClusterCount].EmClstrState == (U1)1)
 			{
-				S2_PrcGU_X = stg_InLyr_SodDtctClstr[S1_PrcGU_SodClusterCount].st_crd[S1_PrcGU_SodPointCount].In_X + 1000;
-				S2_PrcGU_Y = stg_InLyr_SodDtctClstr[S1_PrcGU_SodClusterCount].st_crd[S1_PrcGU_SodPointCount].In_Y + 500;
-				stg_Prc_ArGridInfo[S2_PrcGU_Y][S2_PrcGU_X].CnSodDtct = 1;
-				stg_Prc_ArGridInfo[S2_PrcGU_Y][S2_PrcGU_X].InOcpy += 5;
+				s2t_PrcGU_X = stg_InLyr_SodDtctClstr[s1t_PrcGU_SodClusterCount].st_crd[s1t_PrcGU_SodCrdCount].In_X + (S2)1000;
+				s2t_PrcGU_Y = stg_InLyr_SodDtctClstr[s1t_PrcGU_SodClusterCount].st_crd[s1t_PrcGU_SodCrdCount].In_Y + (S2)500;
+				stg_Prc_ArGridInfo[s2t_PrcGU_Y][s2t_PrcGU_X].CnSodDtct = (U1)1;
+				stg_Prc_ArGridInfo[s2t_PrcGU_Y][s2t_PrcGU_X].InOcpy += (U1)5;
 			}
 		}
 	}
 
 	// 物体確定
-	for (U2_PrcGU_LoopRow = 0; U2_PrcGU_LoopRow < 1000; U2_PrcGU_LoopRow++)
+	for (u2t_PrcGU_LoopRow = (U2)0; u2t_PrcGU_LoopRow < (U2)1000; u2t_PrcGU_LoopRow++)
 	{
-		for (U2_PrcGU_LoopColumn = 0; U2_PrcGU_LoopColumn < 4000; U2_PrcGU_LoopColumn++)
+		for (u2t_PrcGU_LoopColumn = (U2)0; u2t_PrcGU_LoopColumn < (U2)4000; u2t_PrcGU_LoopColumn++)
 		{
-			if (stg_Prc_ArGridInfo[U2_PrcGU_LoopRow][U2_PrcGU_LoopColumn].InOcpy >= 16)
+			if (stg_Prc_ArGridInfo[u2t_PrcGU_LoopRow][u2t_PrcGU_LoopColumn].InOcpy >= (U1)16)
 			{
-				stg_Prc_ArGridInfo[U2_PrcGU_LoopRow][U2_PrcGU_LoopColumn].CnObjSet = 1;
+				stg_Prc_ArGridInfo[u2t_PrcGU_LoopRow][u2t_PrcGU_LoopColumn].CnObjSet = (U1)1;
 			}
 		}
 	}
