d25cc2f79b25763efde01b5d5a558b573000e2a2
Mao Migita <m.29.migi@gmail.com>
2024-03-22 14:22:08 +0900

    vds_InLyr_CapSodClstr()のユニットテストを追加

--- ApFsn_SoftComp/InLyr.h
+++ ApFsn_SoftComp/InLyr.h
@@ -4,6 +4,7 @@
 // 公開関数
 extern void vdg_InLyr_init(void);
 extern void vdg_InLyr_GlobalCapture(void);
+extern void vds_InLyr_CapSodClstr(st_pvm_clstr(*psts_InLyrCSC_InClstr), st_sod_clstr(*psts_InLyrCSC_OutClstr));
 
 // 外部公開変数
 extern st_snr_clstr stg_InLyr_SnrDtctClstr[3]; // ソナーセンサ検知クラスタ
--- ApFsn_SoftComp_unittest/InLyr_unittest.cpp
+++ ApFsn_SoftComp_unittest/InLyr_unittest.cpp
@@ -38,6 +38,9 @@ TEST(TestCaseInLyrGC, GlobalCapture) {
 	stg_pvm_dtct_clstr[0].st_dtct_crd[3].In_X = -200;
 	stg_pvm_dtct_clstr[0].st_dtct_crd[3].In_Y = -300;
 	stg_pvm_dtct_clstr[0].InCrdNum = 4;
+	stg_pvm_dtct_clstr[1].st_dtct_crd[0].In_X = 100;
+	stg_pvm_dtct_clstr[1].st_dtct_crd[0].In_Y = 100;
+	stg_pvm_dtct_clstr[1].InCrdNum = 1;
 
 	vdg_InLyr_GlobalCapture();
 
@@ -47,4 +50,34 @@ TEST(TestCaseInLyrGC, GlobalCapture) {
 	EXPECT_EQ(-200, stg_InLyr_SodDtctClstr[0].st_crd[3].In_X);
 	EXPECT_EQ(-300, stg_InLyr_SodDtctClstr[0].st_crd[3].In_Y);
 	EXPECT_EQ(4, stg_InLyr_SodDtctClstr[0].InCrdNum);
+	EXPECT_EQ(100, stg_InLyr_SodDtctClstr[1].st_crd[0].In_X);
+	EXPECT_EQ(100, stg_InLyr_SodDtctClstr[1].st_crd[0].In_Y);
+	EXPECT_EQ(1, stg_InLyr_SodDtctClstr[1].InCrdNum);
+}
+
+//vds_InLyr_CapSodClstr()のユニットテスト
+TEST(TestCaseInLyrCSC, CapSodClstr) {
+
+	st_pvm_clstr* psts_PvmClstr; // 外部データのPVM検知クラスタ用のポインタ
+	st_sod_clstr* psts_SodClstr; // InLyrのPVM検知クラスタ用のポインタ
+
+	psts_PvmClstr = &stg_pvm_dtct_clstr[0];
+	psts_SodClstr = &stg_InLyr_SodDtctClstr[0];
+	
+	//PVW
+	psts_PvmClstr->st_dtct_crd[0].In_X = (S2)100;
+	psts_PvmClstr->st_dtct_crd[0].In_Y = (S2)200;
+	psts_PvmClstr->InCrdNum = (U1)1;
+	psts_PvmClstr->EmClstrState = (U1)1;
+	psts_SodClstr->st_crd[0].In_X = (S2)0;
+	psts_SodClstr->st_crd[0].In_Y = (S2)0;
+	psts_SodClstr->InCrdNum = (U1)0;
+	psts_SodClstr->EmClstrState = (U1)0;
+
+	vds_InLyr_CapSodClstr(psts_PvmClstr,psts_SodClstr);
+
+	EXPECT_EQ(100, psts_SodClstr->st_crd[0].In_X);
+	EXPECT_EQ(200, psts_SodClstr->st_crd[0].In_Y);
+	EXPECT_EQ(1, psts_SodClstr->InCrdNum);
+	EXPECT_EQ(1, psts_SodClstr->EmClstrState);
 }
