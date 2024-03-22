862c21b73c13715b95236e9ed825a7a19d83f2fb
Mao Migita <m.29.migi@gmail.com>
2024-03-22 15:03:55 +0900

    「構造体＋配列アドレス渡し」の実装     Prc.cpp に関数追加(vds_Prc_SetObj())

--- ApFsn_SoftComp/Prc.cpp
+++ ApFsn_SoftComp/Prc.cpp
@@ -5,8 +5,11 @@
 // プロトタイプ宣言
 void vdg_Prc_init(void);
 void vdg_Prc_GridUpdate(void);
+void vds_Prc_SetObj(st_GridMap *psts_PrcSO_GridMapInfo[1000][4000]);
 
 st_GridMap stg_Prc_ArGridInfo[1000][4000]; // グリッドマップ情報
+
+st_GridMap *psts_PrcSO_ArGridInfo[1000][4000]; // グリッドマップ情報のポインタ
 
 // 初期化
 void vdg_Prc_init(void)
@@ -22,6 +25,8 @@ void vdg_Prc_init(void)
 			stg_Prc_ArGridInfo[u2t_PrcI_LoopRow][u2t_PrcI_LoopColumn].CnSodDtct = (U1)0;
 			stg_Prc_ArGridInfo[u2t_PrcI_LoopRow][u2t_PrcI_LoopColumn].InOcpy = (U1)0;
 			stg_Prc_ArGridInfo[u2t_PrcI_LoopRow][u2t_PrcI_LoopColumn].CnObjSet = (U1)0;
+
+			psts_PrcSO_ArGridInfo[u2t_PrcI_LoopRow][u2t_PrcI_LoopColumn] = &stg_Prc_ArGridInfo[u2t_PrcI_LoopRow][u2t_PrcI_LoopColumn];
 		}
 	}
 
@@ -34,8 +39,6 @@ void vdg_Prc_GridUpdate(void)
 	S1 s1t_PrcGU_SnrCrdCount;
 	S1 s1t_PrcGU_SodClusterCount;
 	S1 s1t_PrcGU_SodCrdCount;
-	U2 u2t_PrcGU_LoopRow;
-	U2 u2t_PrcGU_LoopColumn;
 	U2 u2t_PrcGU_X = (U2)0;
 	U2 u2t_PrcGU_Y = (U2)0;
 
@@ -50,8 +53,8 @@ void vdg_Prc_GridUpdate(void)
 				u2t_PrcGU_Y = stg_InLyr_SnrDtctClstr[s1t_PrcGU_SnrClusterCount].st_crd[s1t_PrcGU_SnrCrdCount].In_Y + (U2)500;
 				stg_Prc_ArGridInfo[u2t_PrcGU_Y][u2t_PrcGU_X].CnSnrDtct = (U1)1;
 				stg_Prc_ArGridInfo[u2t_PrcGU_Y][u2t_PrcGU_X].InOcpy = stg_Prc_ArGridInfo[u2t_PrcGU_Y][u2t_PrcGU_X].InOcpy + (U1)5;
-				//飽和ガード
-				if (stg_Prc_ArGridInfo[u2t_PrcGU_Y][u2t_PrcGU_X].InOcpy > (U1)250) 
+				// 飽和ガード
+				if (stg_Prc_ArGridInfo[u2t_PrcGU_Y][u2t_PrcGU_X].InOcpy > (U1)250)
 				{
 					stg_Prc_ArGridInfo[u2t_PrcGU_Y][u2t_PrcGU_X].InOcpy = (U1)250;
 				}
@@ -70,7 +73,7 @@ void vdg_Prc_GridUpdate(void)
 				u2t_PrcGU_Y = stg_InLyr_SodDtctClstr[s1t_PrcGU_SodClusterCount].st_crd[s1t_PrcGU_SodCrdCount].In_Y + (U2)500;
 				stg_Prc_ArGridInfo[u2t_PrcGU_Y][u2t_PrcGU_X].CnSodDtct = (U1)1;
 				stg_Prc_ArGridInfo[u2t_PrcGU_Y][u2t_PrcGU_X].InOcpy = stg_Prc_ArGridInfo[u2t_PrcGU_Y][u2t_PrcGU_X].InOcpy + (U1)5;
-				//飽和ガード
+				// 飽和ガード
 				if (stg_Prc_ArGridInfo[u2t_PrcGU_Y][u2t_PrcGU_X].InOcpy > (U1)250)
 				{
 					stg_Prc_ArGridInfo[u2t_PrcGU_Y][u2t_PrcGU_X].InOcpy = (U1)250;
@@ -80,16 +83,26 @@ void vdg_Prc_GridUpdate(void)
 	}
 
 	// 物体確定
+	vds_Prc_SetObj(psts_PrcSO_ArGridInfo);
+
+	return;
+}
+
+void vds_Prc_SetObj(st_GridMap *psts_PrcSO_GridMapInfo[1000][4000])
+{
+	U2 u2t_PrcGU_LoopRow;
+	U2 u2t_PrcGU_LoopColumn;
+
 	for (u2t_PrcGU_LoopRow = (U2)0; u2t_PrcGU_LoopRow < (U2)1000; u2t_PrcGU_LoopRow++)
 	{
 		for (u2t_PrcGU_LoopColumn = (U2)0; u2t_PrcGU_LoopColumn < (U2)4000; u2t_PrcGU_LoopColumn++)
 		{
-			if (stg_Prc_ArGridInfo[u2t_PrcGU_LoopRow][u2t_PrcGU_LoopColumn].InOcpy >= (U1)16)
+			if (psts_PrcSO_GridMapInfo[u2t_PrcGU_LoopRow][u2t_PrcGU_LoopColumn]->InOcpy >= (U1)16)
 			{
-				stg_Prc_ArGridInfo[u2t_PrcGU_LoopRow][u2t_PrcGU_LoopColumn].CnObjSet = (U1)1;
+				psts_PrcSO_GridMapInfo[u2t_PrcGU_LoopRow][u2t_PrcGU_LoopColumn]->CnObjSet = (U1)1;
 			}
 		}
 	}
-
+	
 	return;
 }
